ebc026c6c47d911281e346e100b3fa2cae0f1ad7
yanrong <fooyanrong@gmail.com>
2026-03-17 00:31:12 +0800

    fixed login bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "./lib/auth";
+import { redirect } from "next/navigation";
 import ChatAppLoader from "./components/ChatAppLoader";
 
-export default function Page() {
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) redirect("/login");
   return <ChatAppLoader />;
 }
