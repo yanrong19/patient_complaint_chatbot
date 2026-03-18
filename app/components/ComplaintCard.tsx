8a2fcf836a14446f0a90cb777db6933d8b9f5cc2
yanrong <fooyanrong@gmail.com>
2026-03-19 00:43:34 +0800

    minor UI fix

--- app/components/ComplaintCard.tsx
+++ app/components/ComplaintCard.tsx
@@ -105,7 +105,7 @@ export default function ComplaintCard({ complaint }: ComplaintCardProps) {
         </div>
 
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-          className={`w-3.5 h-3.5 text-slate-400 flex-shrink-0 mt-0.5 transition-transform duration-200 ${expanded ? "rotate-180" : ""}`}>
+          className={`w-3.5 h-3.5 text-slate-400 flex-shrink-0 mt-0.5 transition-transform duration-200 ${expanded ? "" : "rotate-180"}`}>
           <path fillRule="evenodd" d="M9.47 6.47a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 1 1-1.06 1.06L10 8.06l-3.72 3.72a.75.75 0 0 1-1.06-1.06l4.25-4.25Z" clipRule="evenodd" />
         </svg>
       </button>
